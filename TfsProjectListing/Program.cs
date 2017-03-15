374e2389edc4995a84f709ff5fc400f69e4c5cb9
BakalDev <bakaldev@gmail.com>
2017-03-15 12:14:45 +0000

    Editable version

--- TfsProjectListing/Program.cs
+++ TfsProjectListing/Program.cs
@@ -12,20 +12,20 @@ namespace TfsProjectListing
 {
 	class Program
 	{
-		public static string TFSServer = "http://fcctfs.cccs.co.uk:8080/tfs/basic";
-
+		public static string TFSServer = "http://fcctfs.cccs.co.uk:8080/tfs/basic"; // remember to add port number to server :8080/tfs/
+		public static string FileOutputPath = @"";
 		static void Main(string[] args)
 		{
 			string serverName = TFSServer;
 
-			//1.Construct the server object
+			// Construct the server object
 			TfsTeamProjectCollection tfs = new TfsTeamProjectCollection(new Uri(serverName));
 			VersionControlServer vcs = tfs.GetService<VersionControlServer>();
 
-			//2.Query all root branches
+			// Query root branches
 			BranchObject[] bos = vcs.QueryRootBranchObjects(RecursionType.Full);
-					
-			//3.Display all the root branches
+
+			// Can be used to display each root branch
 			//Array.ForEach(bos, (bo) => DisplayAllBranches(bo, vcs));
 
 			// 4. Write to file
@@ -34,26 +34,23 @@ namespace TfsProjectListing
 			TextWriter oldOut = Console.Out;
 			try
 			{
-				ostrm = new FileStream(@"\\ldsfileproapp01\Home$\adambd\Visual Studio 2012\Projects\TfsProjectListing\TfsProjectListing\TFSListing.txt", FileMode.OpenOrCreate, FileAccess.Write);
+				ostrm = new FileStream(string.Format("{0}TFSListing.txt", FileOutputPath), FileMode.OpenOrCreate, FileAccess.Write);
 				writer = new StreamWriter(ostrm);
 			}
 			catch (Exception e)
 			{
-				Console.WriteLine("Cannot open Redirect.txt for writing");
+				Console.WriteLine("Cannot open TFSListing.txt for writing");
 				Console.WriteLine(e.Message);
 				return;
 			}
 			Console.SetOut(writer);
-
-
-			// grab each item name where not deleted
+			
+			// Grab each item name where not deleted
 			foreach (var item in bos.OrderBy(bo => bo.Properties.RootItem.Item))
 			{
+				// ensure item is not already marked as deleted
 				if (!item.Properties.RootItem.IsDeleted)
-				{
 					Console.WriteLine(item.Properties.RootItem.Item);
-				}
-				
 			}
 
 			Console.SetOut(oldOut);
@@ -61,31 +58,36 @@ namespace TfsProjectListing
 			ostrm.Close();
 		}
 
-		private static void DisplayAllBranches(BranchObject bo, VersionControlServer vcs)
-		{
-			//0.Prepare display indentation
-			for (int tabcounter = 0; tabcounter < 1; tabcounter++)
-				Console.Write("\t");
+		///// <summary>
+		///// Queries all branches against branch object
+		///// </summary>
+		///// <param name="bo"></param>
+		///// <param name="vcs"></param>
+		//private static void DisplayAllBranches(BranchObject bo, VersionControlServer vcs)
+		//{
+		//	//0.Prepare display indentation
+		//	for (int tabcounter = 0; tabcounter < 1; tabcounter++)
+		//		Console.Write("\t");
 
-			//1.Display the current branch
-			Console.WriteLine(string.Format("{0}", bo.Properties.RootItem.Item));
+		//	//1.Display the current branch
+		//	Console.WriteLine(string.Format("{0}", bo.Properties.RootItem.Item));
 
-			//2.Query all child branches (one level deep)
-			BranchObject[] childBos = vcs.QueryBranchObjects(bo.Properties.RootItem, RecursionType.OneLevel);
+		//	//2.Query all child branches (one level deep)
+		//	BranchObject[] childBos = vcs.QueryBranchObjects(bo.Properties.RootItem, RecursionType.OneLevel);
 
-			//2.1 Order branch objects
+		//	//2.1 Order branch objects
 
-			//3.Display all children recursively
-			//recursionlevel++;
-			foreach (BranchObject child in childBos)
-			{
-				if (child.Properties.RootItem.Item == bo.Properties.RootItem.Item)
-					continue;
+		//	//3.Display all children recursively
+		//	//recursionlevel++;
+		//	foreach (BranchObject child in childBos)
+		//	{
+		//		if (child.Properties.RootItem.Item == bo.Properties.RootItem.Item)
+		//			continue;
 
-				DisplayAllBranches(child, vcs);
-			}
-			//recursionlevel--;
-		}
+		//		DisplayAllBranches(child, vcs);
+		//	}
+		//	//recursionlevel--;
+		//}
 
 		//private static int recursionlevel = 0;
 	}
